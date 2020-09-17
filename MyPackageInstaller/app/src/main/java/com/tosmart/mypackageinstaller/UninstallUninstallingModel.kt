083321776afca109cebc04392f03f1e311f06bde
ggz <miaoyun@MacBook-Pro.local>
2020-09-17 15:02:30 +0800

    rm useless file

--- MyPackageInstaller/app/src/main/java/com/tosmart/mypackageinstaller/UninstallUninstallingModel.kt
+++
@@ -1,8 +0,0 @@
-package com.tosmart.mypackageinstaller
-
-/**
- * @author ggz
- * @date 2020/8/11
- */
-class UninstallUninstallingModel {
-}
--- MyPackageInstaller/app/src/main/java/com/tosmart/mypackageinstaller/uninstall/UninstallConfirmFragment.kt
+++
@@ -1,8 +0,0 @@
-package com.tosmart.mypackageinstaller.uninstall
-
-/**
- * @author ggz
- * @date 2020/8/11
- */
-class UninstallConfirmFragment {
-}
